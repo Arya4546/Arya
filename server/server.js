48a03536bc4d8a4a0980cf635954a27ddf4c6dda
Arya Deep Singh <singharya9693@gmail.com>
2025-08-25 13:55:00 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,11 +7,14 @@ const app = express();
 
 
 app.use(cors({
-  origin: 'https://arya-gules.vercel.app',
-  methods: ['GET', 'POST'],
-  allowedHeaders: ['Content-Type'],
+  origin: "https://arya-gules.vercel.app",
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
 
+// Handle preflight requests
+app.options("*", cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
